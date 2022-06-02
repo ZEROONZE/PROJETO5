98cbeb812317c27aeec92a2970f4a153f75d2ba7
ZEROONZE <luke_adm@hotmail.com>
2022-06-02 00:48:47 -0300

    Alinhamento de arquivos

--- JavaScript/JavaScript1.js
+++ JavaScript/JavaScript1.js
@@ -1,18 +1,4 @@
 
-let contador = 1; 
-
-setInterval( function() {
-
-document.getElementById('slide' + contador).checked = true;
-contador++;
-
-if(contador > 5) {
-	contador = 1;
-}
-
-}, 3000)
-
-
 
 
 
